efae8ac029beff037223a6ddea9021bb0d49a6d6
SofiaMari <sofiamari1999@gmail.com>
2025-03-04 14:36:39 +0100

    Updated gitignore, removed .docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/sofia/Scrivania/RestTestGen-Wiki/node_modules/infima/dist/css/default/default.css"),
-  require("/home/sofia/Scrivania/RestTestGen-Wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/sofia/Scrivania/RestTestGen-Wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/sofia/Scrivania/RestTestGen-Wiki/src/css/custom.css"),
-];
